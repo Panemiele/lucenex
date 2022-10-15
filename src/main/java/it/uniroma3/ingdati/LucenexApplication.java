cb8990ab8920b0c3d5ed7f51bf8a07b241864ad9
Panemiele <gabriele.romualdi98@gmail.com>
2022-10-15 17:52:34 +0200

    Feature - Initial commit - initialize the Springboot project

---
+++ src/main/java/it/uniroma3/ingdati/LucenexApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.ingdati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucenexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucenexApplication.class, args);
+	}
+
+}
